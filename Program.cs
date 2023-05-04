1115bfa32bde6875f27d3fc9d983f5b6c1aa91db
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-05-04 16:53:10 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LabClases2.Clases;
+
+namespace LabClases2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            A a = b;
+            a.F();
+            b.F();
+            a.G();
+            b.G();
+
+            Console.ReadKey();
+
+        }
+    }
+}
